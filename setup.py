50c508acc81d30115677b84630455f61c217ef5e
DapperDog <DapperDog@users.noreply.github.com>
2017-12-02 21:44:13 -0500

    Create setup.py
    just adding this because its easier for a friend

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name='PokemonTCGOCodeChecker',
+      version='1.0',
+      description='Just a pokemon tcgo code checker',
+      url='https://github.com/DapperDog/PokemonTCGOCodeChecker',
+      author='Alex Zacharuk',
+      author_email='zacharukalexander@gmail.com',
+      license='MIT',
+      install_requires=[
+          'requests',
+          'configparser',
+      ])
